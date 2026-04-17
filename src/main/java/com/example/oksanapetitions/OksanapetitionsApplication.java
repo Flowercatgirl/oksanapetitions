af51dde58b29c85169af040977049568a798050a
Flowercatgirl <oksana.pankeviciene@gmail.com>
2026-04-17 17:23:30 +0100

    Initial Spring Boot project with War packaging

---
+++ src/main/java/com/example/oksanapetitions/OksanapetitionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.oksanapetitions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OksanapetitionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OksanapetitionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/oksanapetitions/OksanapetitionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oksanapetitions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OksanapetitionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
